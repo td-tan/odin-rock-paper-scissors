4f2f079d77ff3508b7b02859a0366ffd36355d45
td-tan <47648230+td-tan@users.noreply.github.com>
2023-01-10 12:49:57 +0100

    refactor(playSingleRound): use math to simplify win-lose-draw conditions

--- script.js
+++ script.js
@@ -11,31 +11,39 @@ function getComputerChoice() {
 }
 
 function playSingleRound(playerSelection, computerSelection = getComputerChoice().toLowerCase()) {
+
+    // Use simple math to determine the win conditions
+    // Wins: -2, 1, 1
+    // Lose: -1, -1, 2
+    // Draw: 0 
+    let result = (playerSelection) => {
+        let choice_values = {
+            "Rock": 1,
+            "Paper": 2,
+            "Scissors": 3
+        };
+
+        sum = choice_values(playerSelection) - choice_values[computerSelection];
+
+        switch(sum) {
+            case 0:
+                return "Draw!";
+            case -2:
+            case 1:
+                return `You Win! ${playerSelection} beats ${computerSelection}`;
+            case -1:
+            case 2:
+                return `You Win! ${playerSelection} beats ${computerSelection}`;
+        }
+    };
+
     switch(playerSelection.toLowerCase()) {
         case "rock":
-            if (computerSelection === "paper") {
-                return "You Lose! Paper beats Rock";
-            } else if (computerSelection === "scissors") {
-                return "You Win! Rock beats Scissors";
-            } else {
-                return "Draw!"
-            }
+            return result("rock");
         case "paper":
-            if (computerSelection === "scissors") {
-                return "You Lose! Scissors beats Paper";
-            } else if (computerSelection === "rock") {
-                return "You Win! Paper beats Rock";
-            } else {
-                return "Draw!"
-            }
+            return result("paper");
         case "scissors":
-            if (computerSelection === "rock") {
-                return "You Lose! Rock beats Scissors";
-            } else if (computerSelection === "paper") {
-                return "You Win! Scissors beats Rock";
-            } else {
-                return "Draw!"
-            }
+            return result("scissors");
         default:
             return "Invalid input!"
     }
