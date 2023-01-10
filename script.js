39892af61dafadb132085bbaa7316a223dbc2615
td-tan <47648230+td-tan@users.noreply.github.com>
2023-01-10 12:20:30 +0100

    feat(game): implement single round for player

--- script.js
+++ script.js
@@ -10,7 +10,33 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
-// Test ten rounds
-for (let i = 0; i < 10; i++) {
-    console.log(getComputerChoice());
+function playSingleRound(playerSelection, computerSelection = getComputerChoice().toLowerCase()) {
+    switch(playerSelection.toLowerCase()) {
+        case "rock":
+            if (computerSelection === "paper") {
+                return "You Lose! Paper beats Rock";
+            } else if (computerSelection === "scissors") {
+                return "You Win! Rock beats Scissors";
+            } else {
+                return "Draw!"
+            }
+        case "paper":
+            if (computerSelection === "scissors") {
+                return "You Lose! Scissors beats Paper";
+            } else if (computerSelection === "rock") {
+                return "You Win! Paper beats Rock";
+            } else {
+                return "Draw!"
+            }
+        case "scissors":
+            if (computerSelection === "rock") {
+                return "You Lose! Rock beats Scissors";
+            } else if (computerSelection === "paper") {
+                return "You Win! Scissors beats Rock";
+            } else {
+                return "Draw!"
+            }
+        default:
+            return "Invalid input!"
+    }
 }
