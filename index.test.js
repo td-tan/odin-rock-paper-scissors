7cf0162b46c3f65e1563f540e98078a99d0d18d5
td-tan <47648230+td-tan@users.noreply.github.com>
2023-01-13 20:28:03 +0100

    Revert "test(playRound): add test for player win"
    This reverts commit eb4ab96d5e515e0dc3f54793d46fdeddcd498b53.

--- index.test.js
+++ index.test.js
@@ -2,9 +2,6 @@ const playRound = require('./index');
 
 describe('playRound', () => {
     test('Player selects Rock and Computer selects Paper', () => {
-        expect(playRound("Rock", "Paper")).toBe("You Lose! Paper beats Rock");
-    });
-    test('Player selects Paper and Computer selects Rock', () => {
-        expect(playRound("Paper", "Rock")).toBe("You Win! Paper beats Rock");
+      expect(playRound("Rock", "Paper")).toBe("You Lose! Paper beats Rock");
     });
 });
