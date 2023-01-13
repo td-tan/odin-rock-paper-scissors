a3e450846ba5ae18df35b9ccaab0645dc743bf55
td-tan <47648230+td-tan@users.noreply.github.com>
2023-01-13 21:08:26 +0100

    test(playRound): add test case for win-lose-draw

--- index.js
+++ index.js
@@ -74,6 +74,22 @@ function runTests() {
         console.assert(hitChoices.Rock !== 0 || hitChoices.Paper !== 0 || hitChoices.Scissors !== 0, "Test random choices");
     };
 
+    const testplayRound = () => {
+        const testcase = 'testplayRound';
+        console.assert(playRound('Rock', 'Scissors') === 'You Win! Rock beats Scissors', `${testcase} => Test Player Win`);
+        console.assert(playRound('Paper', 'Rock') === 'You Win! Paper beats Rock', `${testcase} => Test Player Win`);
+        console.assert(playRound('Scissors', 'Paper') === 'You Win! Scissors beats Paper', `${testcase} => Test Player Win`);
+
+        console.assert(playRound('Scissors', 'Rock') === 'You Lose! Rock beats Scissors', `${testcase} => Test Player Lose`);
+        console.assert(playRound('Rock', 'Paper') === 'You Lose! Paper beats Rock', `${testcase} => Test Player Lose`);
+        console.assert(playRound('Paper', 'Scissors') === 'You Lose! Scissors beats Paper', `${testcase} => Test Player Lose`);
+
+        console.assert(playRound('Rock', 'Rock') === 'Draw!', `${testcase} => Test Draw`);
+        console.assert(playRound('Paper', 'Paper') === 'Draw!', `${testcase} => Test Draw`);
+        console.assert(playRound('Scissors', 'Scissors') === 'Draw!', `${testcase} => Test Draw`);
+    };
+
 
     testGetComputerChoice();
+    testplayRound();
 }
