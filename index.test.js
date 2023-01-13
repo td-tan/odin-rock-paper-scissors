50fe3dac74f62458ea30a675efc37aa64ecbb0d4
td-tan <47648230+td-tan@users.noreply.github.com>
2023-01-13 16:47:17 +0100

    test(playRound): add test for draw

--- index.test.js
+++ index.test.js
@@ -7,4 +7,7 @@ describe('playRound', () => {
     test('Player selects Paper and Computer selects Rock', () => {
         expect(playRound("Paper", "Rock")).toBe("You Win! Paper beats Rock");
     });
+    test('Player selects Paper and Computer selects Paper', () => {
+        expect(playRound("Paper", "Paper")).toBe("Draw!");
+    });
 });
