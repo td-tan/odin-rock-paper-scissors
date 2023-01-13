767ec4bc1c9bc406e9d28e3c88a41b24d8bf5498
td-tan <47648230+td-tan@users.noreply.github.com>
2023-01-13 20:25:56 +0100

    Revert "test(playRound): add test case for scissors vs rock"
    This reverts commit 11ec538a43ea33c99f19e57a2d181ac487c5e721.

--- index.test.js
+++ index.test.js
@@ -10,14 +10,4 @@ describe('playRound', () => {
     test('Player selects Paper and Computer selects Paper', () => {
         expect(playRound("Paper", "Paper")).toBe("Draw!");
     });
-
-    test('Player selects Scissors and Computer selects Rock', () => {
-        expect(playRound("Scissors", "Rock")).toBe("You Lose! Scissors beats Rock");
-    });
-    test('Player selects Rock and Computer selects Scissors', () => {
-        expect(playRound("Rock", "Scissors")).toBe("You Win! Scissors beats Rock");
-    });
-    test('Player selects Rock and Rock selects Paper', () => {
-        expect(playRound("Rock", "Rock")).toBe("Draw!");
-    });
 });
