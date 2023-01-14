33c7c0cf8e3f4aebad298a04c5f2cd838e3fb0a9
td-tan <47648230+td-tan@users.noreply.github.com>
2023-01-14 20:38:53 +0100

    test: remove old

--- index.js
+++ index.js
@@ -23,45 +23,6 @@ const game = {
     }
 };
 
-
-function runTests() {
-
-    const testGetComputerChoice = () => {
-        const testcase = 'testGetComputerChoice';
-        let hitChoices = {
-            'Rock': 0,
-            'Paper': 0,
-            'Scissors': 0
-        };
-        for (let i = 0; i < 100; i++) {
-            const randomChoice = getComputerChoice();
-            
-            hitChoices[randomChoice]++;
-        }
-        console.log(hitChoices);
-        console.assert(hitChoices.Rock !== 0 || hitChoices.Paper !== 0 || hitChoices.Scissors !== 0, `${testcase} => Test random choices`);
-    };
-
-    const testplayRound = () => {
-        const testcase = 'testplayRound';
-        console.assert(playRound('Rock', 'Scissors') === 'You Win! Rock beats Scissors', `${testcase} => Test Player Win`);
-        console.assert(playRound('Paper', 'Rock') === 'You Win! Paper beats Rock', `${testcase} => Test Player Win`);
-        console.assert(playRound('Scissors', 'Paper') === 'You Win! Scissors beats Paper', `${testcase} => Test Player Win`);
-
-        console.assert(playRound('Scissors', 'Rock') === 'You Lose! Rock beats Scissors', `${testcase} => Test Player Lose`);
-        console.assert(playRound('Rock', 'Paper') === 'You Lose! Paper beats Rock', `${testcase} => Test Player Lose`);
-        console.assert(playRound('Paper', 'Scissors') === 'You Lose! Scissors beats Paper', `${testcase} => Test Player Lose`);
-
-        console.assert(playRound('Rock', 'Rock') === 'Draw!', `${testcase} => Test Draw`);
-        console.assert(playRound('Paper', 'Paper') === 'Draw!', `${testcase} => Test Draw`);
-        console.assert(playRound('Scissors', 'Scissors') === 'Draw!', `${testcase} => Test Draw`);
-    };
-
-
-    testGetComputerChoice();
-    testplayRound();
-}
-
 function display(result) {
     const output = document.querySelector('div#results');
     output.textContent = result;
